c9ab8df30e36fd9d680f64feffb6fe39bbe390dc
yjk <yjk8654@outlook.com>
2018-08-14 16:48:49 +0800

    hello

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello{
 	public static void main(String args[]) {
 		System.out.println("Hello Git");
 		System.out.println("Hellnihasdasdo Git");
-
+		System.out.println("Hellnihasdasdo Git");
 	}
 }
