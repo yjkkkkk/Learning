4f3444a49ec146a7eeaa12d8bcb019d6da009cec
yjk <yjk8654@outlook.com>
2018-08-14 16:49:17 +0800

    Emp

---
+++ Emp.java
@@ -0,0 +1,7 @@
+public class hello{
+	public static void main(String args[]) {
+		System.out.println("Hello Git");
+		System.out.println("Hellnihasdasdo Git");
+		System.out.println("Hellnihasdasdo Git");
+	}
+}
